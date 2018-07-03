5146b83506f807ef47146482841c7372749f1c87
Sam Tolmay <samtolmay@gmail.com>
2018-07-03 22:26:02 +0200

    Hello World improvements

--- hello/README.md
+++ hello/README.md
@@ -1,6 +1,10 @@
 ### Solidity Hello World
 
-Deployed on the Ropsten Test Network at: 
+V0.1 deployed on the Ropsten Test Network at:
 ```
 0x1a4a91e11311fa7ceacede1d996843df2359adb3
+```
+V0.2 deployed on the Ropsten Test Network at:
+```
+0x03bc3365f6209aa180741df37a4fdb32f5fe012a
 ```
--- hello/hello.sol
+++ hello/hello.sol
@@ -1,13 +1,21 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.22;
+
+
+/// @title A Hello World contract
+/// @author Sam Tolmay
 contract Hello {
-    string message;
+
+    /// @dev Solidity automatically creates a getter function since we declare this public
+    string public message;
+
+    /// @param _message The message to initialise this contract with
     constructor(string _message) public {
         message = _message;
     }
-    function set(string _message) public {
+
+    /// @notice Set the contract's message to a different message
+    /// @param _message The message to set the contract message variable to
+    function set(string _message) external {
         message = _message;
-    }
-    function get() public constant returns (string) {
-        return message;
     }
 }
