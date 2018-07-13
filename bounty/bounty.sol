f90322918fa09976b1f2b2678efe85e9a84f4213
Sam Tolmay <samtolmay@gmail.com>
2018-07-13 23:31:28 +0200

    bounty
    improve data structure
    add answer functions and events

--- bounty/bounty.sol
+++ bounty/bounty.sol
@@ -4,33 +4,53 @@ pragma solidity ^0.4.22;
 /// @author Sam Tolmay
 contract Bounty {
 
+    event NewQuestion(uint questionId, string title, string body);
+    event NewAnswer(uint questionId, uint answerId, string answer);
+
     address private owner;
 
     struct Question {
-        address poster;
         string title;
         string body;
+        string[] answers;
+        mapping (uint => address) questionAnswerBy;
+        address acceptedAnswer;
     }
-
-    struct Answer {
-        address respondent;
-        string body;
-    }
-
-    mapping (uint => address) public questionAskedBy;
-    mapping (uint => address) public questionAnsweredBy;
-    mapping (uint => uint) public questionAnswersQuestion;
 
     Question[] public questions;
-    Answer[] public answers;
+    mapping (uint => address) public questionAskedBy;
+
 
     constructor() public {
         owner = msg.sender;
     }
 
-    function askQuestion(string _title, string _body) public {
-        questions.push(Question(msg.sender, _title, _body));
+    function askQuestion(string _title, string _body) public returns ( uint ) {
+        Question memory q;
+        q.title = _title;
+        q.body = _body;
+        uint id = questions.push(q) -1;
+        questionAskedBy[id] = msg.sender;
+        emit NewQuestion(id, _title, _body);
+        return id;
+    }
 
+    function answerQuestion(uint _questionId, string _answer) public {
+        Question storage q = questions[_questionId];
+        uint answerId;
+        answerId = q.answers.push(_answer) - 1;
+        q.questionAnswerBy[answerId] = msg.sender;
+        emit NewAnswer(_questionId, answerId, _answer);
+    }
+
+    function getAnswer(uint _questionId, uint _answerId) public view returns (string) {
+        return questions[_questionId].answers[_answerId];
+    }
+
+    function acceptAnswer(uint _questionId, uint _answerId) public {
+        require(msg.sender == questionAskedBy[_questionId]);
+        Question storage q = questions[_questionId];
+        q.acceptedAnswer = q.questionAnswerBy[_answerId];
     }
 
 }
